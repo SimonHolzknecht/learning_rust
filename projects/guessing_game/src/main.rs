c677a793fd96080f2619c86532cc849f00cf6d24
SimonHolzknecht <simon@holzknechtonline.de>
2025-08-19 05:55:27 +0200

    Finished chapter 2, proceeding with chapter 3

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -9,8 +9,6 @@ fn main()
 
   let secret_number = rand::thread_rng().gen_range(1..=100);
 
-  println!("The secret number is {secret_number}");
-
 
   loop
   {
@@ -22,7 +20,7 @@ fn main()
       .read_line(&mut guess)
       .expect("Failed to read line");
 
-    let guess : u32 = guess.trim().parse() {
+    let guess : u32 = match guess.trim().parse() {
       Ok(num) => num,
       Err(_) => {
         println!("Please type a valid number!");
